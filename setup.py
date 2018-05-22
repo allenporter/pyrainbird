968b3a1745fbcea540179455bf317b36c0a534f3
Gregory Benner <gregbenner1@gmail.com>
2018-05-22 15:03:13 -0400

    clean up setup files and fix publish

--- setup.py
+++ setup.py
@@ -1,23 +1,17 @@
-from setuptools import setup, find_packages
-from codecs import open
-from os import path
-
-here = path.abspath(path.dirname(__file__))
-
-# Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 setup(
 
     name='pyrainbird',
-    version='0.1.4',
+    version='0.1.5',
     description='Rain Bird Controller',
-    long_description=long_description,
 
     install_requires=['pycrypto'],
 
-    packages=find_packages(exclude=('tests', 'docs')),
+    packages=['pyrainbird'],
 
     #The project's main homepage.
     url='https://github.com/jbarrancos/pyrainbird/',
