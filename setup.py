1385453af72661479bbf457c6eeb0506e4932483
Gregory Benner <gregbenner1@gmail.com>
2018-05-22 14:14:52 -0400

    bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 setup(
 
     name='pyrainbird',
-    version='0.1.3',
+    version='0.1.4',
     description='Rain Bird Controller',
     long_description=long_description,
 
