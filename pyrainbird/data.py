641f032984b33d47d8b7a629037401739ff0dc4b
Allen Porter <allen@thebends.org>
2022-06-21 18:37:57 -0700

    Fix flake8 errors

--- pyrainbird/data.py
+++ pyrainbird/data.py
@@ -14,7 +14,7 @@ class Pageable(object):
         return isinstance(o, Pageable) and self.page == o.page
 
     def __ne__(self, o):
-        return not __eq__(o)
+        return not self.__eq__(o)
 
     def __str__(self):
         return "page: %d" % self.page
@@ -31,7 +31,7 @@ class Echo(object):
         return isinstance(o, Echo) and self.echo == o.echo
 
     def __ne__(self, o):
-        return not __eq__(o)
+        return not self.__eq__(o)
 
     def __str__(self):
         return "echo: %02X" % self.echo
@@ -50,7 +50,7 @@ class CommandSupport(Echo):
         )
 
     def __ne__(self, o):
-        return not __eq__(o)
+        return not self.__eq__(o)
 
     def __hash__(self):
         return hash((super(CommandSupport, self).__hash__(), self.support))
@@ -82,7 +82,7 @@ class ModelAndVersion(object):
         )
 
     def __ne__(self, o):
-        return not __eq__(o)
+        return not self.__eq__(o)
 
     def __str__(self):
         return "model: %04X, version: %d.%d" % (
@@ -119,7 +119,7 @@ class States(object):
         )
 
     def __ne__(self, o):
-        return not __eq__(o)
+        return not self.__eq__(o)
 
     def __str__(self):
         result = ()
@@ -144,7 +144,7 @@ class AvailableStations(Pageable):
         )
 
     def __ne__(self, o):
-        return not __eq__(o)
+        return not self.__eq__(o)
 
     def __str__(self):
         return "available stations: %X, %s" % (
@@ -169,7 +169,7 @@ class WaterBudget(object):
         )
 
     def __ne__(self, o):
-        return not __eq__(o)
+        return not self.__eq__(o)
 
     def __str__(self):
         return "water budget: program: %d, hi: %02X, lo: %02X" % (
