0d224b6bce8f7a81590d2f44d1913defb9703c2d
Allen Porter <allen@thebends.org>
2022-11-12 09:09:26 -0800

    Update more flake8 errors

--- pyrainbird/resources/__init__.py
+++ pyrainbird/resources/__init__.py
@@ -1,11 +1,10 @@
-import json
 import yaml
-
-from pkg_resources import resource_string, resource_stream
+from pkg_resources import resource_stream
 
 # parameters in number of nibbles (based on string representations of SIP bytes), total lengths in number of SIP bytes
-
-# Changed in json version 3.9: The keyword argument encoding has been removed.
-# https://docs.python.org/3/library/json.html
-RAINBIRD_COMMANDS = yaml.load(resource_stream('pyrainbird.resources', 'sipcommands.yaml'), Loader=yaml.FullLoader)
-RAINBIRD_MODELS = yaml.load(resource_stream('pyrainbird.resources', 'models.yaml'), Loader=yaml.FullLoader)
+RAINBIRD_COMMANDS = yaml.load(
+    resource_stream("pyrainbird.resources", "sipcommands.yaml"), Loader=yaml.FullLoader
+)
+RAINBIRD_MODELS = yaml.load(
+    resource_stream("pyrainbird.resources", "models.yaml"), Loader=yaml.FullLoader
+)
