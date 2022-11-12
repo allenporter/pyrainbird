6636793f76bf04a6f722780f9adbc3db3fdd82c4
Allen Porter <allen@thebends.org>
2022-11-12 09:03:48 -0800

    Install pre-commit and fix flake8 errors

--- pyrainbird/__init__.py
+++ pyrainbird/__init__.py
@@ -4,14 +4,15 @@ import time
 from functools import reduce
 
 from pyrainbird.data import (
-    ModelAndVersion,
+    _DEFAULT_PAGE,
     AvailableStations,
     CommandSupport,
+    ModelAndVersion,
     States,
     WaterBudget,
-    _DEFAULT_PAGE,
 )
 from pyrainbird.resources import RAINBIRD_COMMANDS
+
 from . import rainbird
 from .client import RainbirdClient
 
@@ -49,9 +50,7 @@ class RainbirdController:
     def get_available_stations(self, page=_DEFAULT_PAGE):
         mask = (
             "%%0%dX"
-            % RAINBIRD_COMMANDS["ControllerResponses"]["83"]["setStations"][
-                "length"
-            ]
+            % RAINBIRD_COMMANDS["ControllerResponses"]["83"]["setStations"]["length"]
         )
         return self._process_command(
             lambda resp: AvailableStations(
@@ -63,39 +62,29 @@ class RainbirdController:
 
     def get_command_support(self, command):
         return self._process_command(
-            lambda resp: CommandSupport(
-                resp["support"], echo=resp["commandEcho"]
-            ),
+            lambda resp: CommandSupport(resp["support"], echo=resp["commandEcho"]),
             "CommandSupport",
             command,
         )
 
     def get_serial_number(self):
-        return self._process_command(
-            lambda resp: resp["serialNumber"], "SerialNumber"
-        )
+        return self._process_command(lambda resp: resp["serialNumber"], "SerialNumber")
 
     def get_current_time(self):
         return self._process_command(
-            lambda resp: datetime.time(
-                resp["hour"], resp["minute"], resp["second"]
-            ),
+            lambda resp: datetime.time(resp["hour"], resp["minute"], resp["second"]),
             "CurrentTime",
         )
 
     def get_current_date(self):
         return self._process_command(
-            lambda resp: datetime.date(
-                resp["year"], resp["month"], resp["day"]
-            ),
+            lambda resp: datetime.date(resp["year"], resp["month"], resp["day"]),
             "CurrentDate",
         )
 
     def water_budget(self, budget):
         return self._process_command(
-            lambda resp: WaterBudget(
-                resp["programCode"], resp["seasonalAdjust"]
-            ),
+            lambda resp: WaterBudget(resp["programCode"], resp["seasonalAdjust"]),
             "WaterBudget",
             budget,
         )
@@ -125,9 +114,7 @@ class RainbirdController:
         return self.zones.active(zone)
 
     def set_program(self, program):
-        return self._process_command(
-            lambda resp: True, "ManuallyRunProgram", program
-        )
+        return self._process_command(lambda resp: True, "ManuallyRunProgram", program)
 
     def test_zone(self, zone):
         return self._process_command(lambda resp: True, "TestStations", zone)
@@ -137,27 +124,21 @@ class RainbirdController:
             lambda resp: True, "ManuallyRunStation", zone, minutes
         )
         self._update_irrigation_state()
-        return response == True and self.zones.active(zone)
+        return response is True and self.zones.active(zone)
 
     def stop_irrigation(self):
         response = self._process_command(lambda resp: True, "StopIrrigation")
         self._update_irrigation_state()
-        return response == True and not reduce(
-            (lambda x, y: x or y), self.zones.states
-        )
+        return response is True and not reduce((lambda x, y: x or y), self.zones.states)
 
     def get_rain_delay(self):
-        return self._process_command(
-            lambda resp: resp["delaySetting"], "RainDelayGet"
-        )
+        return self._process_command(lambda resp: resp["delaySetting"], "RainDelayGet")
 
     def set_rain_delay(self, days):
         return self._process_command(lambda resp: True, "RainDelaySet", days)
 
     def advance_zone(self, param):
-        return self._process_command(
-            lambda resp: True, "AdvanceStation", param
-        )
+        return self._process_command(lambda resp: True, "AdvanceStation", param)
 
     def get_current_irrigation(self):
         return self._process_command(
@@ -170,9 +151,7 @@ class RainbirdController:
         self.logger.debug("Request to line: " + str(data))
         decrypted_data = self.rainbird_client.request(
             data,
-            RAINBIRD_COMMANDS["ControllerCommands"]["%sRequest" % command][
-                "length"
-            ],
+            RAINBIRD_COMMANDS["ControllerCommands"]["%sRequest" % command]["length"],
         )
         self.logger.debug("Response from line: " + str(decrypted_data))
         if decrypted_data is None:
@@ -188,9 +167,9 @@ class RainbirdController:
             raise Exception(
                 "Status request failed with wrong response! Requested %s but got %s:\n%s"
                 % (
-                    RAINBIRD_COMMANDS["ControllerCommands"][
-                        "%sRequest" % command
-                    ]["response"],
+                    RAINBIRD_COMMANDS["ControllerCommands"]["%sRequest" % command][
+                        "response"
+                    ],
                     decrypted_data[:2],
                     decoded,
                 )
@@ -203,11 +182,9 @@ class RainbirdController:
         return (
             funct(response)
             if response is not None
-               and response["type"]
-               == RAINBIRD_COMMANDS["ControllerResponses"][
-                RAINBIRD_COMMANDS["ControllerCommands"][cmd + "Request"][
-                    "response"
-                ]
+            and response["type"]
+            == RAINBIRD_COMMANDS["ControllerResponses"][
+                RAINBIRD_COMMANDS["ControllerCommands"][cmd + "Request"]["response"]
             ]["type"]
             else response
         )
@@ -215,9 +192,7 @@ class RainbirdController:
     def _update_irrigation_state(self, page=_DEFAULT_PAGE):
         mask = (
             "%%0%dX"
-            % RAINBIRD_COMMANDS["ControllerResponses"]["BF"]["activeStations"][
-                "length"
-            ]
+            % RAINBIRD_COMMANDS["ControllerResponses"]["BF"]["activeStations"]["length"]
         )
         result = self._process_command(
             lambda resp: States((mask % resp["activeStations"])[:4]),
