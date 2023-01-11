528bdec70b2a0f4499d3d03278bcf92b51bda5b7
Allen Porter <allen.porter@gmail.com>
2023-01-10 23:04:58 -0800

    Add commands for setting date and time (#83)
    Add separate commands for setting the date and time of the device. This
    requires rewriting the encoding/decoding libraries using the new
    sipcommand format that can specify arguments in the same format as the
    responses.
    
    Fixes #47
    
    Example command:
    ```
    $ python3 examples/rainbird_tool.py --log-level=debug set_current_date 2023-01-09
    DEBUG:pyrainbird.async_client:Request to line: 130917E7
    DEBUG:pyrainbird.async_client:Request: {"id": 1673419656.7081575, "jsonrpc": "2.0", "method": "tunnelSip", "params": {"data": "130917E7", "length": 4}}
    DEBUG:pyrainbird.async_client:Response: {"jsonrpc": "2.0", "result":{"length":2, "data":"0113"}, "id": 0}
    DEBUG:pyrainbird.async_client:Response from line: 0113
    DEBUG:pyrainbird.async_client:Response: {'type': 'AcknowledgeResponse', 'commandEcho': 19}
    None
    $ python3 examples/rainbird_tool.py --log-level=debug get_current_date
    DEBUG:pyrainbird.async_client:Request to line: 12
    DEBUG:pyrainbird.async_client:Request: {"id": 1673419658.5084407, "jsonrpc": "2.0", "method": "tunnelSip", "params": {"data": "12", "length": 1}}
    DEBUG:pyrainbird.async_client:Response: {"jsonrpc": "2.0", "result":{"length":4, "data":"920917E7"}, "id": 0}
    DEBUG:pyrainbird.async_client:Response from line: 920917E7
    DEBUG:pyrainbird.async_client:Response: {'type': 'CurrentDateResponse', 'day': 9, 'month': 1, 'year': 2023}
    2023-01-09
    ```

--- examples/rainbird_tool.py
+++ examples/rainbird_tool.py
@@ -12,26 +12,33 @@ $ ./rainbird_tool get_wifi_settings
 ```
 """
 
-import asyncio
-import aiohttp
-import sys
-import inspect
 import argparse
-import logging
-
-from pyrainbird import async_client
+import asyncio
+import datetime
+import inspect
 import logging
 import os
+from typing import Any
 
+import aiohttp
+
+from pyrainbird import async_client
 
 _LOGGER = logging.getLogger(__name__)
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--log-level', default='warning', choices=['debug', 'info', 'warning', 'error', 'critical'], help='The log level')
+    parser.add_argument(
+        "--log-level",
+        default="warning",
+        choices=["debug", "info", "warning", "error", "critical"],
+        help="The log level",
+    )
 
-    subcommand_parsers = parser.add_subparsers(title='Commands', dest='command', required=True)
+    subcommand_parsers = parser.add_subparsers(
+        title="Commands", dest="command", required=True
+    )
     for method_name in dir(async_client.AsyncRainbirdController):
         if method_name.startswith("_"):
             continue
@@ -47,7 +54,9 @@ def parse_args():
             continue
 
         # Create a parser for the method
-        method_parser = subcommand_parsers.add_parser(method_name, help=f'Call the {method_name} method')
+        method_parser = subcommand_parsers.add_parser(
+            method_name, help=f"Call the {method_name} method"
+        )
         method_parser.set_defaults(func=method)
 
         # Get the arguments of the method
@@ -60,9 +69,21 @@ def parse_args():
     return parser.parse_args()
 
 
+def parse_value(value: Any) -> Any:
+    """Parse the command line arg into a value."""
+    try:
+        return datetime.date.fromisoformat(value)
+    except ValueError:
+        pass
+    try:
+        return datetime.time.fromisoformat(value)
+    except ValueError:
+        pass
+    return int(value, 16)
+
+
 async def main():
     args = parse_args()
-    print(getattr(logging, args.log_level.upper()))
     logging.basicConfig(level=getattr(logging, args.log_level.upper()))
 
     host = os.environ["RAINBIRD_SERVER"]
@@ -70,10 +91,15 @@ async def main():
 
     async with aiohttp.ClientSession() as session:
         client = async_client.CreateController(session, host, password)
-        method_args = {k: v for k, v in vars(args).items() if k != 'func' and k != 'command' and k != 'log_level'}
+        method_args = {
+            k: parse_value(v)
+            for k, v in vars(args).items()
+            if k != "func" and k != "command" and k != "log_level"
+        }
         result = await args.func(client, **method_args)
         print(result)
 
+
 # Run the main function if this script is run
-if __name__ == '__main__':
+if __name__ == "__main__":
     asyncio.run(main())
